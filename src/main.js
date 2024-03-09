89773aedb3a25f5a2c5db1d516a9faa267a5b7e6
oksana12345678 <oksana.tanashuk@gmail.com>
2024-03-09 09:39:41 +0100

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { fetchImages } from './js/pixabay-api';
 import './css/loader-styles.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { renderGallery, galleryElement } from './js/render-functions';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 
 const searchForm = document.querySelector('.form');
 const inputElement = document.querySelector('.search-input');
